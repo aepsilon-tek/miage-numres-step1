53b1b6242a018000fd318e9c03bdff23bc874ff0
ayoub <ayoubhofr@gmail.com>
2024-12-10 16:06:52 +0100

    initiate

--- api-springboot/src/main/java/org/aepsilon/ApiSpringbootApplication.java
+++ api-springboot/src/main/java/org/aepsilon/ApiSpringbootApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ApiSpringbootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiSpringbootApplication.class, args);
+		SpringApplication.run(ApiSpringbootApplication.class, args); 
 	}
 
 }
